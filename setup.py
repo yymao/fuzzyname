1e4a22840468b6d995291761f512e0d6b6ed890f
Yao-Yuan Mao <yymao.astro@gmail.com>
2020-03-01 20:29:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,28 +3,37 @@
 A simple Python class for easier name matching (especially in academia)
 Project website: https://github.com/yymao/fuzzyname
 The MIT License (MIT)
-Copyright (c) 2018 Yao-Yuan Mao (yymao)
+Copyright (c) 2018-2020 Yao-Yuan Mao (yymao)
 http://opensource.org/licenses/MIT
 """
 
-from setuptools import setup
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), "fuzzyname", "version.py")) as f:
+    exec(f.read())  # pylint: disable=W0122
 
 setup(
-    name='fuzzyname',
-    version='0.1.0',
-    description='A simple Python class for easier name matching (especially in academia).',
-    url='https://github.com/yymao/fuzzyname',
-    download_url = 'https://github.com/yymao/fuzzyname/archive/v0.1.0.zip',
-    author='Yao-Yuan Mao',
-    author_email='yymao.astro@gmail.com',
-    maintainer='Yao-Yuan Mao',
-    maintainer_email='yymao.astro@gmail.com',
-    license='MIT',
+    name="fuzzyname",
+    version=__version__,  # pylint: disable=E0602 # noqa: F821
+    description="A simple Python class for easier name matching (especially in academia).",
+    url="https://github.com/yymao/fuzzyname",
+    download_url="https://github.com/yymao/fuzzyname/archive/{}.zip".format(
+        __version__  # pylint: disable=E0602 # noqa: F821
+    ),
+    author="Yao-Yuan Mao",
+    author_email="yymao.astro@gmail.com",
+    maintainer="Yao-Yuan Mao",
+    maintainer_email="yymao.astro@gmail.com",
+    license="MIT",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    keywords='fuzzyname',
-    py_modules=['fuzzyname'],
+    keywords="fuzzyname",
+    packages=find_packages(),
+    install_requires=["Unidecode"],
 )

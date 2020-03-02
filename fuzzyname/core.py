8244c50d083355dae895949ba32df6ad752f4b6e
Yao-Yuan Mao <yymao.astro@gmail.com>
2020-03-02 10:37:42 -0500

    Make the match even fuzzier

--- fuzzyname/core.py
+++ fuzzyname/core.py
@@ -27,29 +27,29 @@ class Name(object):
     """
 
     def __init__(self, name, exact=False, use_unidecode=True):
+        self.name = name
+
         if use_unidecode:
             if not _HAS_UNIDECODE:
                 raise RuntimeError(
                     "Cannot import unidecode package. Please install unidecode or set `use_unidecode` to False."
                 )
-            self.name = unidecode(name)
-        else:
-            self.name = name
+            name = unidecode(name)
 
-        self.name = self.name.strip()
-        if "," in self.name:
-            self.name = "{0[1]} {0[0]}".format(self.name.split(","))
+        name = re.sub(r"[^a-z-,.\s]", "", name.strip().lower())
 
-        self.name = self.name.strip()
+        if "," in name:
+            name = "{0[1]} {0[0]}".format(name.split(",")).strip()
+
         self.names = tuple(
-            (
-                n.lower()
-                for n in self.name.replace("-", " ").replace(".", ". ").split()
-                if n != "."
-            )
+            (n for n in name.replace("-", " ").replace(".", ". ").split() if n != ".")
         )
-        assert self.names, "*name* must not be empty"
+
+        if not self.names:
+            raise ValueError("`name` must not be empty")
+
         self.exact = bool(exact)
+        self.normalized_name = " ".join(self.names)
 
     def __repr__(self):
         return repr(self.name)
@@ -61,49 +61,67 @@ class Name(object):
         return not self.__ne__(other)
 
     def __ne__(self, other):
+        if self.normalized_name == other.normalized_name:
+            return False
+
         exact = self.exact or other.exact
 
         if exact and len(self.names) != len(other.names):
             return True
 
-        i = None
-        for i, (a, b) in enumerate(zip(reversed(self.names), reversed(other.names))):
-            if a != b:
-                if not i:
-                    return True
+        if (
+            not exact
+            and (
+                self.normalized_name in other.normalized_name
+                and " " in self.normalized_name
+            )
+            or (
+                other.normalized_name in self.normalized_name
+                and " " in other.normalized_name
+            )
+        ):
+            return False
+
+        matched_back = 0
+        for a, b in zip(reversed(self.names), reversed(other.names)):
+            if a == b:
+                matched_back += 1
+            else:
                 break
 
-        s = slice(None, None if i is None else -i)  # pylint: disable=E1130
-
-        j = None
-        for j, (a, b) in enumerate(zip(self.names[s], other.names[s])):
+        s = slice(None, -matched_back if matched_back else None)
+        matched_front = 0
+        for a, b in zip(self.names[s], other.names[s]):
             if a == b:
+                matched_front += 1
                 continue
-
-            if exact and not (a.endswith(".") or b.endswith(".")):
-                return True
 
             a = a.rstrip(".")
             b = b.rstrip(".")
+            initials = (len(a) == 1 or len(b) == 1)
+
+            if exact and not initials:
+                return True
+
             if not (a.startswith(b) or b.startswith(a)):
-                # check nicknames
-                if len(a) > 1 and len(b) > 1:
+                if initials:
+                    return True
+                else:  # check nicknames
                     for m in re.findall(_re_template.format(a), nicknames, re.M):
                         if b in m.split(","):
                             break
                     else:
                         return True
-                else:
+
+            matched_front += 1
+
+        matched = matched_front + matched_back
+        if matched < max(2, min(len(self.names), len(other.names))):
+            s = slice(matched_front, -matched_back if matched_back else None)
+            for n in (self.names, other.names):
+                n = n[s]
+                if n and not all(len(a.rstrip(".")) == 1 for a in n):
                     return True
-
-        s = slice(
-            j if j is None else j + 1, i if i is None else -i  # pylint: disable=E1130
-        )
-
-        for n in (self.names, other.names):
-            n = n[s]
-            if n and not all(a.endswith(".") for a in n):
-                return True
 
         return False
 
